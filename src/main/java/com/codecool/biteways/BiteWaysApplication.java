f7beec12d73226a6006eab08ac22b25bffda6bf0
rabbitST <biroattila.eu@gmail.com>
2023-04-24 18:25:13 +0200

    Initial commit

---
+++ src/main/java/com/codecool/biteways/BiteWaysApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.biteways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteWaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiteWaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/biteways/BiteWaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.biteways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteWaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
