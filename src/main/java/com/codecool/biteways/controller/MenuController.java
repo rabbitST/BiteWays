220b931e97802178c7890bb2da7254d163db9cc9
rabbitST <biroattila.eu@gmail.com>
2023-04-27 12:02:38 +0200

    add MenuController.java

---
+++ src/main/java/com/codecool/biteways/controller/MenuController.java
@@ -0,0 +1,55 @@
+package com.codecool.biteways.controller;
+
+import com.codecool.biteways.model.Menu;
+import com.codecool.biteways.service.MenuService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/biteways/menu")
+public class MenuController {
+
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    @PostMapping
+    public Menu saveMenu(@RequestBody Menu menu) {
+        return menuService.saveMenu(menu);
+    }
+
+    @GetMapping
+    public List<Menu> findAllMenu() {
+        return menuService.findAllMenu();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Menu findMenuById(@PathVariable("id") Long id) {
+        return menuService.findMenuById(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    public Menu updateMenu(
+            @PathVariable("id") Long id,
+            @RequestBody Menu menu
+    ) {
+        return menuService.updateMenu(id, menu);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteMenu(
+            @PathVariable("id") Long id
+    ) {
+        menuService.deleteMenu(id);
+    }
+
+    @GetMapping(value = "/createmenu")
+    public Menu createMenu() {
+        return menuService.createMenu();
+    }
+
+
+}
--- src/main/java/com/codecool/biteways/service/MenuService.java
+++ src/main/java/com/codecool/biteways/service/MenuService.java
@@ -34,8 +34,8 @@ public class MenuService {
         return menu;
     }
 
-    public void saveMenu(Menu menu) {
-        menuRepository.save(menu);
+    public Menu saveMenu(Menu menu) {
+        return menuRepository.save(menu);
     }
 
     public List<Menu> findAllMenu() {
