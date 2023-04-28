2845433c1696d19cc9c142161f8bc6db754dd28f
rabbitST <biroattila.eu@gmail.com>
2023-04-29 00:13:16 +0200

    add test environment -> application.properties, schema.sql, data.sql

---
+++ src/test/resources/NO_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO recipe(name,downloaded,instructions) values('VEGAN WAFFLES 1',35, 'Preheat waffle iron according to manufacturer’s instructions.\n
+In a large bowl, whisk together the flour, baking powder, salt and sugar. \n
+Stir until just combined with a large wooden spoon, being careful not to over mix. The batter should be fairly thick, if it''s too runny add a few tablespoons more of flour.\n
+Spray waffle iron with non-stick spray. Pour the recommended amount of batter onto hot waffle iron, and cook until the waffle is golden brown on both sides.\n');
+
+
+INSERT INTO ingredient(name,quantity,unit_type,recipe_id) values('flour',500,'G',1);
+INSERT INTO ingredient(name,quantity,unit_type,recipe_id) values('sugar',220,'G',1);
+INSERT INTO ingredient(name,quantity,unit_type,recipe_id) values('Baking Powder',1,'PACKAGE',1);
+INSERT INTO ingredient(name,quantity,unit_type,recipe_id) values('Unsweetened Vanilla Almond Milk',500,'G',1);
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS ingredient;
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS menu;
+DROP TABLE IF EXISTS menu_recipe;
+
+CREATE TABLE recipe
+(
+    id           SERIAL PRIMARY KEY,
+    name         VARCHAR(255),
+    instructions VARCHAR(600),
+    downloaded   INT
+);
+
+CREATE TABLE ingredient
+(
+    id        SERIAL PRIMARY KEY,
+    name      VARCHAR(255),
+    quantity  FLOAT,
+    unit_type VARCHAR(10),
+    recipe_id BIGINT
+);
+
+CREATE TABLE menu
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE menu_recipe
+(
+    id        SERIAL PRIMARY KEY,
+    menu_id   BIGINT,
+    recipe_id BIGINT
+);
+
+ALTER TABLE ingredient
+    ADD CONSTRAINT FK_INGREDIENT_RECIPE FOREIGN KEY (RECIPE_ID) REFERENCES recipe (id);
+-- ALTER TABLE ingredient DROP CONSTRAINT IF EXISTS FK_RECIPEID_RECIPE;
+-- ALTER TABLE ingredient DROP CONSTRAINT ingredient_recipe_id_fkey;
+-- ALTER TABLE ingredient DROP CONSTRAINT fk_recipeid_recipe;
+
