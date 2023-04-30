c98ddb7e5baf162ade0a7d153440ce2d5b6e9d01
rabbitST <biroattila.eu@gmail.com>
2023-04-30 18:03:54 +0200

    add property reading to BiteWaysApplication.java

--- src/main/java/com/codecool/biteways/BiteWaysApplication.java
+++ src/main/java/com/codecool/biteways/BiteWaysApplication.java
@@ -1,8 +1,22 @@
 package com.codecool.biteways;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "${biteWays.api.title}",
+                version = "${biteWays.api.version}",
+                description = "${biteWays.api.description}",
+                license = @License(name = "${biteWays.api.license}"),
+                contact = @Contact(name="${biteWays.api.contact.name}", email = "${biteWays.api.contact.email}")
+        )
+)
 @SpringBootApplication
 public class BiteWaysApplication {
 
