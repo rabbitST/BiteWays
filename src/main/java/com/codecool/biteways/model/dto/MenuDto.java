36cad7d5800872e913646e6b955d701a3ee5480e
rabbitST <biroattila.eu@gmail.com>
2023-05-06 11:55:45 +0200

    Added Frontend Menu page with related pages modifications and some other design mods.

--- src/main/java/com/codecool/biteways/model/dto/MenuDto.java
+++ src/main/java/com/codecool/biteways/model/dto/MenuDto.java
@@ -20,7 +20,7 @@ public class MenuDto {
     private Long id;
 
     @NotBlank(message = "Please provide a name for the menu.")
-    @Size(min = 3, max = 30, message = "Please enter a menu name that is between 3 and 30 characters in length.")
+    @Size(min = 3, max = 50, message = "Please enter a menu name that is between 3 and 30 characters in length.")
     @Pattern(regexp = "^[a-zA-Z0-9\\s\\-.]*$", message = "The menu name can only contain letters, numbers, hyphens, spaces.")
     private String name;
     @Valid
