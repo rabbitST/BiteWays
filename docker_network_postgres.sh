dcc42d805e031bd841a164a63ef73a8d7c3ae885
rabbitST <biroattila.eu@gmail.com>
2023-05-08 16:45:18 +0200

    Docker network solution. (docker_network_postgres.sh)

---
+++ docker_network_postgres.sh
@@ -0,0 +1,7 @@
+docker network create bitewaysnetwork
+
+docker run --name bitewaysdb --network  bitewaysnetwork -e POSTGRES_DB=biteways -e POSTGRES_USER=bitewaysuser -e POSTGRES_PASSWORD=12345 -p 5442:5432 -d postgres:alpine
+
+docker build -t biteways_img .
+
+docker run -p8080:8080 --network bitewaysnetwork -e DB_URL=bitewaysdb -e DB_PORT=5432 -it biteways_img
