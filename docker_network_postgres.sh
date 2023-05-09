082ee778792f76404306a47f5a468197056eceea
rabbitST <biroattila.eu@gmail.com>
2023-05-09 16:12:42 +0200

    add completion to docker_network_postgres.sh

--- docker_network_postgres.sh
+++ docker_network_postgres.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker network create bitewaysnetwork
 
 docker run --name bitewaysdb --network  bitewaysnetwork -e POSTGRES_DB=biteways -e POSTGRES_USER=bitewaysuser -e POSTGRES_PASSWORD=12345 -p 5442:5432 -d postgres:alpine
