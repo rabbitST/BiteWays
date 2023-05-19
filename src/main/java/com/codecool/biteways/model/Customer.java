2e1f86b5d202de7a0e86b56c727331fd19d0a64b
rabbitST <biroattila.eu@gmail.com>
2023-05-19 18:57:41 +0200

    add fixes Customer, Chef

--- src/main/java/com/codecool/biteways/model/Customer.java
+++ src/main/java/com/codecool/biteways/model/Customer.java
@@ -1,10 +1,18 @@
 package com.codecool.biteways.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "customer")
 public class Customer {
